f9551d566dd0ce1b3b341b4f0f70fc2da240e9dd
alex-harv <alexharveycs@proton.me>
2023-02-07 22:52:33 -0600

    experimental bubble code added

--- heap.c
+++ heap.c
@@ -1,95 +1,143 @@
 #include "heap.h"
 
-
 HeapNode heap[256];
 int heapSize;
 
-
 /**********************************************************************************************
  *
- * Declare and define any local function here to support your implementation of the heap 
+ * Declare and define any local function here to support your implementation of the heap
  *
  **********************************************************************************************/
-
-
-
 /******** INSERT YOUR CODE HERE IF NEEDED ********/
 
+HeapNode *parent(int n)
+{
+    // moves binary representation of int one to the right,
+    // halfing the number with floor / accessing parent node of binary tree
+    return &heap[n >> 1];
+}
+HeapNode *lChild(int n)
+{
+    return &heap[n * 2];
+}
+HeapNode *rChild(int n)
+{
+    return &heap[n * 2 + 1];
+}
 
+void bubbleUp(int pos)
+{
+    // int pos = child pos
+    HeapNode temp = *parent(pos);
 
-void bubbleUp(int pos);
-void bubbleDown(int pos);
+    // to bubbleup there must be a child element smaller than the parent
+    if ((*parent(pos)).freq > heap[pos].freq)
+    {
 
- 
+        // swapping the child with the parent will bubble it up
+        *parent(pos) = heap[pos];
+        heap[pos] = temp;
+    }
+}
+void bubbleDown(int pos)
+{
+    // int pos = parent pos
+    // this function operates the same as "heapify" (4041)
+    HeapNode temp = *parent(pos);
+    if (heap[pos].freq < (*lChild(pos)).freq && heap[pos].freq < (*rChild(pos)).freq)
+        return;
+
+    if ((*lChild(pos)).freq < (*rChild(pos)).freq)
+    {
+        temp = *lChild(pos);
+        *lChild(pos) = heap[pos];
+        heap[pos] = temp;
+        bubbleDown(pos * 2);
+    }
+    else
+    {
+        temp = *rChild(pos);
+        *rChild(pos) = heap[pos];
+        heap[pos] = temp;
+        bubbleDown(pos * 2 + 1);
+    }
+}
+// TODO test bubbledown / bubbleup
+
 /*Initialize Heap*/
-void heapInit() {
+void heapInit()
+{
     heapSize = 0;
     heap[0].c = 0;
     heap[0].t_node = NULL;
     heap[0].freq = 0;
 }
 
-/* 
- * To insert a new node into the heap 
+/*
+ * To insert a new node into the heap
  * 1. The size of the heap is increased by 1
- * 2. The new element is inserted as the last element of the heap 
+ * 2. The new element is inserted as the last element of the heap
  * 3. Sift up the new element (swim/bubble up operation) until the heap property has been reestablished.
  */
-void HeapInsert(char c, huffman_tree_node *t, int freq) {
-
+void HeapInsert(char c, huffman_tree_node *t, int freq)
+{
     /******** INSERT YOUR CODE HERE ********/
-
+    heapSize++;
+    heap[heapSize].c = c;
+    heap[heapSize].t_node = t;
+    heap[heapSize].freq = freq;
 }
- 
-/* 
- * heap[1] is the root of the heap and contains the minimum node. To remove root, 
- * 1. the content of heap[1] is copies to an return struct. 
+
+/*
+ * heap[1] is the root of the heap and contains the minimum node. To remove root,
+ * 1. the content of heap[1] is copies to an return struct.
  * 2. the last element of the heap heap[heapSize] is copied to the root.
- * 3. the size of the heap is decreased by 1. 
+ * 3. the size of the heap is decreased by 1.
  * 4. sift down the new root (sink/tri operation) to reestablish the heap property.
  * 5. return the old heap[1] stored in the return struct/
- * Note that, heap[0] is NOT used in this heap implementation 
+ * Note that, heap[0] is NOT used in this heap implementation
  */
-HeapNode DeleteMin() {
+HeapNode DeleteMin()
+{
 
     /******** INSERT YOUR CODE HERE ********/
-
 }
- 
-void heapPrint()  {
-    int  i;
+
+void heapPrint()
+{
+    int i;
     printf("\n----------------\n  HEAP BEGIN\n-----------------\n");
-    for (i = 1; i <= heapSize; i++) {
+    for (i = 1; i <= heapSize; i++)
+    {
         printf("%d ", heap[i].c);
-	if(isprint(heap[i].c)) 
-		printf("(%c)", heap[i].c);
-	else 
-		printf("(--)");
+        if (isprint(heap[i].c))
+            printf("(%c)", heap[i].c);
+        else
+            printf("(--)");
         printf("%d\n", heap[i].freq);
     }
     printf("\n----------------\n  HEAP END\n-----------------\n");
 }
 
-
-
 /****************************************************************
  *
  * Debuggine Code for the heap data structure
- * Compile this code with 
+ * Compile this code with
  *
-****************************************************************/
+ ****************************************************************/
 
 #ifdef TEST
 
-int main() {
+int main()
+{
     heapInit();
 
-        HeapInsert('a', NULL, 44);
-        HeapInsert('b', NULL, 32);
-        HeapInsert('d', NULL, 99);
-        HeapInsert('f', NULL, 43);
-        HeapInsert('u', NULL, 51);
-        HeapInsert('y', NULL, 1);
+    HeapInsert('a', NULL, 44);
+    HeapInsert('b', NULL, 32);
+    HeapInsert('d', NULL, 99);
+    HeapInsert('f', NULL, 43);
+    HeapInsert('u', NULL, 51);
+    HeapInsert('y', NULL, 1);
 
     heapPrint();
 
