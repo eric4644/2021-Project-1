8f493feb265b6d691c9ac4d7dd629277dd5eee03
eric4644 <125217931+eric4644@users.noreply.github.com>
2023-02-26 21:21:24 -0600

    Update code.c

--- code.c
+++ code.c
@@ -1,11 +1,11 @@
 #include "heap.h"
 #include "tree.h"
-
+ 
 #ifndef CODE_H
 #define CODE_H
 
 char huffman_code[256][20];
-char code_buffer[20];
+char code_buffer[20]; //store current location
 
 /**********************************************************************************************
  *
@@ -13,38 +13,61 @@ char code_buffer[20];
  *
  **********************************************************************************************/
 
-void init_code()
-{
+ 
+void init_code()  {
 	int i;
-	for (i = 0; i < 256; i++)
-	{
-		huffman_code[i][0] = 0;
+	for(i = 0; i < 256; i++)  {
+ 		huffman_code[i][0] = 0;
 	}
 }
-
-void print_code()
-{
+ 
+void print_code()   {
 	printf("\n----------------\n  CODES BEGIN\n-----------------\n");
-	for (int i = 0; i < 256; i++)
-	{
-		if (huffman_code[i][0] != 0)
-		{
-			if (isprint(i))
-				printf("%d (%c): %s\n", i, i, huffman_code[i]);
+
+        for(int i = 0; i < 256; i++)  {
+                if(huffman_code[i][0] != 0)  {
+			if(isprint(i))
+                        	printf("%d (%c): %s\n", i, i, huffman_code[i]);
 			else
-				printf("%d (--): %s\n", i, huffman_code[i]);
+                        	printf("%d (--): %s\n", i, huffman_code[i]);
 		}
-	}
+        }
 	printf("\n----------------\n  CODES END\n-----------------\n");
 }
 
-/*
+/* 
  * Traverse the huffman tree and store the huffman codes in huffman_code.
- * You may use code_buffer as a temp variable to store partial results.
+ * You may use code_buffer as a temp variable to store partial results. 
  */
-void gen_code(huffman_tree_node *node, int pos)
-{
-	/******* INSERT YOUR CODE HERE **********/
+
+//src as buffer_code and dest 
+void stringCopy(char* src, char* dest){
+	printf("%s\n", src);
+	for(int i = 0; i < 20; i++){
+		dest[i] = src[i];
+	}
 }
 
-#endif // CODE_H
+void gen_code(huffman_tree_node *node, int pos)
+{
+     /******* INSERT YOUR CODE HERE **********/	
+	//recursively run though the tree node
+	if(node -> left != NULL){ 
+		code_buffer[pos] = '0';
+		gen_code(node -> left, pos+1);
+	}
+
+	if(node -> right != NULL){
+		code_buffer[pos] = '1';
+		gen_code(node -> right, pos+1);
+	}
+	
+	if(node -> right == NULL && node -> left == NULL){
+		stringCopy(code_buffer, huffman_code[node -> c]);
+		}
+
+		code_buffer[pos] = NULL;
+
+}
+
+#endif  // CODE_H
